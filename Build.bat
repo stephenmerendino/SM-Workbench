541dbcb1942a47f989a078a55ed091e8322c50ca
Stephen Merendino <stephenmerendino@gmail.com>
2025-11-26 12:21:27 -0800

    More project setup

--- Build.bat
+++ Build.bat
@@ -1,8 +1,13 @@
 @echo off
 
+SETLOCAL
+
 set BaseFilename=Main
 set MainDir=%~dp0
+set SrcDir=%MainDir%Src\
 set BuildDir=%MainDir%Build\
+
+call %MainDir%sm-engine\Build.bat
 
 mkdir %BuildDir% >nul 2>&1
 
@@ -10,7 +15,9 @@ set ExeOutput=%BuildDir%%BaseFilename%.exe
 set PdbOutput=%BuildDir%%BaseFilename%.pdb
 set ObjOutput=%BuildDir%%BaseFilename%.obj
 
-set FilesToCompile=%MainDir%%BaseFilename%.cpp
+set FilesToCompile=%SrcDir%%BaseFilename%.cpp
 REM set FilesToCompile=%FilesToCompile%" "%MainDir%SomeNewFile.cpp
 
 cl /nologo /Zi /Od %FilesToCompile% /Fe%ExeOutput% /Fd%PdbOutput% /Fo%ObjOutput%
+
+ENDLOCAL
---
+++ Src/Main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "../sm-engine/Src/Test.cpp"
+
+#include "sm-engine/Renderer/Renderer.h"
+
+int main()
+{
+    TestPrint();
+    printf("Hello World 2\n");
+}
