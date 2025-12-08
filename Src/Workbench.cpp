6d62dbccf7dedbfa81bad000569409630f396900
stephenmerendino <stephenmerendino@gmail.com>
2025-12-08 15:37:32 -0800

    Update Engine

--- Src/Workbench.cpp
+++ Src/Workbench.cpp
@@ -2,21 +2,21 @@
 
 #include "SM/Engine.h"
 #include "SM/Platform.h"
+#include "SM/Renderer/VulkanRenderer.h"
 
+SM::VulkanRenderer s_renderer;
 SM::Platform::Window* s_window = nullptr;
 
 static void Init()
 {
     SM::EngineConfig config {
-        .m_dllName = "Workbench.dll",
-        .m_windowName = "Workbench",
-        .m_windowWidth = 1920,
-        .m_windowHeight = 1080,
         .m_rawAssetsDir = "..\\..\\..\\RawAssets\\"
     };
-
     SM::Init(config);
+
     s_window = SM::Platform::OpenWindow("Workbench", 1920, 1080);
+    s_renderer.Init(s_window);
+    s_renderer.SetClearColor(SM::ColorF32(100, 0, 0));
 }
 
 static void MainLoop()
@@ -24,8 +24,11 @@ static void MainLoop()
     bool quit = false;
     while(!quit)
     {
-        SM::Platform::UpdateWindow(s_window);
-        bool quit = SM::ExitRequested() || SM::Platform::WasKeyPressed(SM::Platform::kKeyEscape);
+        SM::Platform::Update(s_window);
+
+        s_renderer.RenderFrame();
+
+        quit = SM::ExitRequested() || SM::Platform::WasKeyPressed(SM::Platform::kKeyEscape);
     }
 }
 
